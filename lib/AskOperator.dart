8cc17e4e785af81e5ca73ab703faeae51dc0740a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-11 22:08:10 -0700

    botton Icons

--- lib/AskOperator.dart
+++ lib/AskOperator.dart
@@ -3,6 +3,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'PDF/PdfGeneratonScreen.dart';
 import 'Quiz/QuizQuestionScreen.dart';
 import 'customWidget/ButtonIcon.dart';
+                        ButtonIcon(
+                          icon: FontAwesomeIcons.divide,
+                          function: () => Navigator.push(
+                              context,
                               MaterialPageRoute(
                                 builder: (context) => isQuiz
                                     ? const QuizQuestionScreen(
