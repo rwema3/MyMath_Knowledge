07ce420e2869971cffcccd75f7c0020387c9e2ec
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-11 22:12:23 -0700

    MediaQuery size

--- lib/customWidget/ButtonIcon.dart
+++ lib/customWidget/ButtonIcon.dart
@@ -21,6 +21,8 @@ class ButtonIcon extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
           ),
           width: MediaQuery.of(context).size.width > 550
+              ? 200
+              : MediaQuery.of(context).size.width / 2,
           
           ),
         ),
