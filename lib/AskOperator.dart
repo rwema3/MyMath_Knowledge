7910ac6c297c6fecb0238c03676f63e4f114994a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-11 22:08:27 -0700

    The Row in Dart

--- lib/AskOperator.dart
+++ lib/AskOperator.dart
@@ -3,7 +3,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'PDF/PdfGeneratonScreen.dart';
 import 'Quiz/QuizQuestionScreen.dart';
 import 'customWidget/ButtonIcon.dart';
-
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ButtonIcon(
+                          icon: FontAwesomeIcons.xmark,
+                          function: () => Navigator.push(
+                              context,
                               MaterialPageRoute(
                                 builder: (context) => isQuiz
                                     ? const QuizQuestionScreen(
